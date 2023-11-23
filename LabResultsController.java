e3e22014eda09e4b2174256294bc65fdbbe81fbd
Erin Cameron <erincameron@lauras-mbp.home>
2023-11-23 15:59:45 -0500

    eliminate import stmt

--- LabResultsController.java
+++ LabResultsController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 
 public class LabResultsController {
